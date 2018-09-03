9f0905b29094865c6a0f27472f917f8c32422674
daandouwe <daan_douwe@outlook.com>
2018-09-03 20:20:59 +0200

    mkdir data

--- get-data.sh
+++ get-data.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+if [[ ! -d data ]]; then
+  mkdir data
+fi
+
 if [[ ! -d data/wikitext-2 ]]; then
     echo 'Downloading WikiText-2'
-    mkdir -p data
     wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip
     unzip wikitext-2-v1.zip
     rm wikitext-2-v1.zip
